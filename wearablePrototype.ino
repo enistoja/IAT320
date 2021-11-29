bb41f0e26a248d5e15dad5169c683c584413e523
enistoja <71460641+enistoja@users.noreply.github.com>
2021-11-29 06:41:15 -0800

    Created prototype code for rinal
    in theory works to read sensor code, read SD card file based on filename, and play sound

---
+++ wearablePrototype.ino
@@ -0,0 +1,117 @@
+//code for sensor adapted from https://learn.sparkfun.com/tutorials/apds-9960-rgb-and-gesture-sensor-hookup-guide/all
+//code for memory card adapted from https://learn.sparkfun.com/tutorials/microsd-shield-and-sd-breakout-hookup-guide#sd-card-breakout-boards
+//code for playing audio adapted from https://maker.pro/arduino/projects/arduino-audio-player
+//libraries
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+#include <SPI.h>
+#include <SD.h>
+
+#include "TMRpcm.h"
+
+//interrupt pin, SDA SCL Rx and Tx are external interrupt pins, d9 and d10 are pin change interrupt pins
+#define APDS9960_INT    0 // Needs to be an interrupt pin
+
+//define CS pin
+#define SD_ChipSelectPin 10
+
+//define Speaker pin
+#define mySpeakerPin 12;
+
+// Global Variables
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+int isr_flag = 0;
+TMRpcm tmrpcm;
+
+void setup() {
+
+   // Initialize Serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("--------------------------------"));
+  Serial.println(F("SparkFun APDS-9960 - GestureTest"));
+  Serial.println(F("--------------------------------"));
+
+  // Initialize interrupt service routine. First parameter is the interrupt pin, second ISR (function that is gonna be called), mode defines what makes the code change, in this case going from HIGH to LOW
+  //right now it is RX0, recommended to use attachInterrupt() function if this will be ran in other hardware. It is not likely to, so we do Unwise Coding Decisions
+  attachInterrupt(0, interruptRoutine, FALLING);
+
+  //check to see if sensor initializes
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+
+  //check if sensor works
+  // Start running the APDS-9960 gesture sensor engine
+  if ( apds.enableGestureSensor(true) ) {
+    Serial.println(F("Gesture sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during gesture sensor init!"));
+  }
+
+
+  //speaker and SD code
+  tmrpcm.speakerPin = mySpeakerPin;
+  if (!SD.begin(SD_ChipSelectPin))
+  {
+    Serial.println("SD fail");
+    return;
+  }
+
+  tmrpcm.setVolume(6);
+
+}
+
+void loop() {
+
+    //if not interruptible
+  if( isr_flag == 1 ) {
+    //remove interruption
+    detachInterrupt(0);
+    //do gesture
+    handleGesture();
+    //make interruptible
+    isr_flag = 0;
+    //reatach interrupt
+    attachInterrupt(0, interruptRoutine, FALLING);
+  }
+  
+}
+
+//reset interrupt flag
+void interruptRoutine() {
+  isr_flag = 1;
+}
+
+
+//set up gesture actions, this is where we'll read the files we have
+void handleGesture() {
+    if ( apds.isGestureAvailable() ) {
+    switch ( apds.readGesture() ) {
+      case DIR_UP:
+        Serial.println("UP");
+        //sound file!
+        //tmrpcm.play("");
+        break;
+      case DIR_DOWN:
+        Serial.println("DOWN");
+        break;
+      case DIR_LEFT:
+        Serial.println("LEFT");
+        break;
+      case DIR_RIGHT:
+        Serial.println("RIGHT");
+        break;
+      case DIR_NEAR:
+        Serial.println("NEAR");
+        break;
+      case DIR_FAR:
+        Serial.println("FAR");
+        break;
+      default:
+        Serial.println("NONE");
+    }
+  }
+}
